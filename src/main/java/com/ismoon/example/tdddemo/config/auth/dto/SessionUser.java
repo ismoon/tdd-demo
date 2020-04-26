33e2366823d06439bdfd268acc773aeb38f494c3
문인수 <insu4341@gmail.com>
2020-04-26 16:28:38 +0900

    spring-session-jdbc 적용

--- src/main/java/com/ismoon/example/tdddemo/config/auth/dto/SessionUser.java
+++ src/main/java/com/ismoon/example/tdddemo/config/auth/dto/SessionUser.java
@@ -3,8 +3,13 @@ package com.ismoon.example.tdddemo.config.auth.dto;
 import com.ismoon.example.tdddemo.domain.user.User;
 import lombok.Getter;
 
+import java.io.Serializable;
+
 @Getter
-public class SessionUser {
+public class SessionUser implements Serializable {
+
+    private static final long serialVersionUID = -7146547511083676317L;
+
     private String name;
 
     private String email;
