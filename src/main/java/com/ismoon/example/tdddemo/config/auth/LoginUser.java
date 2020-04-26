70d1b9492037c5ad2b9775ab5843542e1ba988c1
문인수 <insu4341@gmail.com>
2020-04-26 15:48:41 +0900

    HandlerMethodArgumentResolver를 이용하여 SessionUser를 가져오도록 코드 리펙토링

---
+++ src/main/java/com/ismoon/example/tdddemo/config/auth/LoginUser.java
@@ -0,0 +1,11 @@
+package com.ismoon.example.tdddemo.config.auth;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LoginUser {
+}
--- src/main/java/com/ismoon/example/tdddemo/web/IndexController.java
+++ src/main/java/com/ismoon/example/tdddemo/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.ismoon.example.tdddemo.web;
 
+import com.ismoon.example.tdddemo.config.auth.LoginUser;
 import com.ismoon.example.tdddemo.config.auth.dto.SessionUser;
 import com.ismoon.example.tdddemo.service.posts.PostsService;
 import lombok.RequiredArgsConstructor;
@@ -8,19 +9,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import javax.servlet.http.HttpSession;
-
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
 
-    public String index(Model model) {
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+    public String index(Model model, @LoginUser SessionUser user) {
 
         if (user != null) {
             model.addAttribute("userName", user.getName());
